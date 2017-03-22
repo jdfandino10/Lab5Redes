094f98f694bf087f7625700d4e348c60de8a4dc7
jdfandino10 <jd.fandino10@uniandes.edu.co>
2017-03-21 21:57:07 -0500

    avances servidor

---
+++ Servidor/src/mundo/Main.java
@@ -0,0 +1,11 @@
+package mundo;
+
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		Servidor s = new Servidor(1000, 50);
+	}
+
+}
