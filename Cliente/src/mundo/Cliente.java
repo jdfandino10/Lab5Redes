4c39d2cbe1e161c28281002aa8c5ea21ffc15488
jdfandino10 <jd.fandino10@uniandes.edu.co>
2017-03-25 13:40:11 -0500

    dispose fix

--- Cliente/src/mundo/Cliente.java
+++ Cliente/src/mundo/Cliente.java
@@ -187,6 +187,10 @@ public class Cliente {
 		}
 	}
 	
+	public boolean socketConnection() {
+		return server.isClosed();
+	}
+	
 	public void closeConnection(){
 		try {
 			out.println(BYE);
--- Cliente/src/mundo/ConnectionChecker.java
+++ Cliente/src/mundo/ConnectionChecker.java
@@ -1,14 +1,14 @@
 package mundo;
 
 public class ConnectionChecker extends Thread{
-	
+
 	private ConnectionStateListener listener;
 	private Cliente c;
 	public ConnectionChecker(ConnectionStateListener listener, Cliente c) {
 		this.listener = listener;
 		this.c = c;
 	}
-	
+
 	public void run() {
 		try {
 			this.sleep(2000);
@@ -17,15 +17,16 @@ public class ConnectionChecker extends Thread{
 			e1.printStackTrace();
 		}
 		while(true){
-			if(!c.downloadHasStarted() || c.isPaused()) listener.changeConnectionState(c.goodConnection());
-			else listener.changeConnectionState(true);
-			try {
-				this.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			if(!c.socketConnection()){
+				if(!c.downloadHasStarted() || c.isPaused()) listener.changeConnectionState(c.goodConnection());
+				else listener.changeConnectionState(true);
+				try {
+					this.sleep(1000);
+				} catch (InterruptedException e) {
+					break;
+				}
 			}
 		}
 	}
-	
+
 }
