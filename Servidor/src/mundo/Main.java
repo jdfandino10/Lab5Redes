1c3420bd088b75720ef44dd4f451ddeddd0093a0
jdfandino10 <jd.fandino10@uniandes.edu.co>
2017-03-26 14:09:23 -0500

    media importancia tiempo

--- Servidor/src/mundo/Main.java
+++ Servidor/src/mundo/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	private static final int MEDIA = 1;
 	private static final int ALTA = 2;
 
-	private static int T_CONEXION = ALTA; //importancia ALTA, MEDIA o BAJA
+	private static int T_CONEXION = MEDIA; //importancia ALTA, MEDIA o BAJA
 	private static int BAJA_LATENCIA = ALTA; //importancia ALTA, MEDIA o BAJA
 	private static int ALTO_ANCHO = MEDIA; //importancia ALTA, MEDIA o BAJA
 	
@@ -29,7 +29,7 @@ public class Main {
 		else if (test500Buffer) buffer*=5; //utiliza el 500%
 		if (test10Tam) tPaquetes*=0.1; //utiliza el 10%
 		else if (test200Tam) tPaquetes*=2; //utiliza el 200%
-		//Servidor(maxConexiones, buffer[en bytes], tamaño paquetes, minutos conexión inactiva)
+		//Servidor(maxConexiones, buffer[en bytes], tamaño paquetes, minutos conexión inactiva, importancia baja conexion, importancia baja latencia, importancia alto ancho banda)
 		Servidor s = new Servidor(maxConexiones, buffer, tPaquetes, minutos, T_CONEXION, BAJA_LATENCIA, ALTO_ANCHO);
 	}
 
